1b7e2e880869e95bcc3087a7ac72936fdb5727e8
ekeneakubue <ekeneakubue@gmail.com>
2024-01-17 22:21:15 +0100

    second commit

--- src/components/services/Service.jsx
+++ src/components/services/Service.jsx
@@ -3,7 +3,7 @@ import './Service.css'
 
 export default function Service() {
   return (
-    <div className='container'>
+    <div className='container-fluid'>
         <div className="service">
             <div className="row">
                 <div className="col-md-12">
@@ -11,43 +11,61 @@ export default function Service() {
                 </div>
             </div>
             <div className="row">
-                <div className="col-md-4 service-bg-light" data-aos="fade-right" data-aos-duration="2000">
-                    <i class="bi bi-bank2 text-danger"></i>
-                    <div>Study/Work Permits</div>
+                <div className="col-md-4 service-col-light" data-aos="fade-right" data-aos-duration="2000">
+                    <div className="service-bg-light">
+                        <i class="bi bi-bank2 text-danger"></i>
+                        <div>Study/Work Permits</div>
+                    </div>
                 </div>
-                <div className="col-md-4 service-bg-white">
-                    <i class="bi bi-airplane-engines-fill text-danger"></i>
-                    <div>Visitor/Travel Visas</div>
+                <div className="col-md-4 service-col-white">
+                    <div className="service-bg-white">
+                        <i class="bi bi-airplane-engines-fill text-danger"></i>
+                        <div>Visitor/Travel Visas</div>
+                    </div>                    
                 </div>
-                <div className="col-md-4 service-bg-light" data-aos="fade-left" data-aos-duration="2000">
-                    <i class="bi bi-amd text-danger"></i>
-                    <div>Refugee/Protected Person</div>
-                </div>
-
-                <div className="col-md-4 service-bg-white">
-                    <i class="bi bi-people-fill text-danger"></i>
-                    <div>Family/Spousal Sponsorship</div>
-                </div>
-                <div className="col-md-4 service-bg-light">
-                    <i class="bi bi-unity text-danger"></i>
-                    <div>Express Entry</div>
-                </div>
-                <div className="col-md-4 service-bg-white">
-                    <i class="bi bi-ticket-detailed-fill text-danger"></i>
-                    <div>PR Citizenship</div>
+                <div className="col-md-4 service-col-light" data-aos="fade-left" data-aos-duration="2000">
+                    <div className="service-bg-light">
+                        <i class="bi bi-amd text-danger"></i>
+                        <div>Refugee/Protected Person</div>
+                    </div>
                 </div>
 
-                <div className="col-md-4 service-bg-light" data-aos="fade-right" data-aos-duration="2000">
-                    <i class="bi bi-tools text-danger"></i>
-                    <div>Worker/Business Programs</div>
+                <div className="col-md-4 service-col-white">
+                    <div className="service-bg-white">
+                        <i class="bi bi-people-fill text-danger"></i>
+                        <div>Family/Spousal Sponsorship</div>
+                    </div>                    
                 </div>
-                <div className="col-md-4 service-bg-white">
-                    <i class="bi bi-steam text-danger"></i>
-                    <div>Provincial Nominee/Skilled Worker</div>                    
+                <div className="col-md-4 service-col-light">
+                    <div className="service-bg-light">
+                        <i class="bi bi-unity text-danger"></i>
+                        <div>Express Entry</div>
+                    </div>
                 </div>
-                <div className="col-md-4 service-bg-light" data-aos="fade-left" data-aos-duration="2000">
-                    <i class="bi bi-tencent-qq text-danger"></i>
-                    <div>TFW</div>
+                <div className="col-md-4 service-col-white">
+                    <div className="service-bg-white">
+                        <i class="bi bi-ticket-detailed-fill text-danger"></i>
+                        <div>PR Citizenship</div>
+                    </div>
+                </div>
+
+                <div className="col-md-4 service-col-light" data-aos="fade-right" data-aos-duration="2000">
+                    <div className="service-bg-light">
+                        <i class="bi bi-tools text-danger"></i>
+                        <div>Worker/Business Programs</div>
+                    </div>                    
+                </div>
+                <div className="col-md-4 service-col-white">
+                    <div className="service-bg-white">
+                        <i class="bi bi-steam text-danger"></i>
+                        <div>Provincial Nominee/Skilled Worker</div>  
+                    </div>                                      
+                </div>
+                <div className="col-md-4 service-col-light" data-aos="fade-left" data-aos-duration="2000">
+                    <div className="service-bg-light" data-aos="fade-left" data-aos-duration="2000">
+                        <i class="bi bi-tencent-qq text-danger"></i>
+                        <div>TFW</div>
+                    </div>
                 </div>                
             </div>
         </div>
