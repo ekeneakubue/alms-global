51a499900539c01d9b42456761061e4db953ce87
ekeneakubue <ekeneakubue@gmail.com>
2024-01-20 11:09:20 +0100

    added scroll to top icon

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,36 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './footer.css';
 
 export default function Footer() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+      // Add a scroll event listener to check whether the user has scrolled down
+      const handleScroll = () => {
+        if (window.scrollY > 200) {
+          setIsVisible(true);
+        } else {
+          setIsVisible(false);
+        }
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+  
+      // Remove the event listener on component unmount
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+  
+    const scrollToTop = () => {
+      // Scroll to the top of the page when the button is clicked
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth', // for a smooth scrolling effect
+      });
+    };
+
   return (
     <div className='container-fluid bg-dark'>
         <div className="footer">
@@ -20,19 +49,19 @@ export default function Footer() {
                     <div className="col-md-3">
                         <div className="f-col">
                             <p className='text-light'>Company</p>
-                            <p><Link to='/'>Home</Link></p>
-                            <p><Link to='/about'>About Us</Link></p>
-                            <p><Link to='/services'>Our Services</Link></p>
-                            <p><Link to='/contact'>Our Contacts</Link></p>                         
+                            <p><Link to='/' onClick={scrollToTop}>Home</Link></p>
+                            <p><Link to='/about' onClick={scrollToTop}>About Us</Link></p>
+                            <p><Link to='/services' onClick={scrollToTop}>Our Services</Link></p>
+                            <p><Link to='/contact' onClick={scrollToTop}>Our Contacts</Link></p>                         
                         </div>
                     </div>
 
                     <div className="col-md-2">
                         <div className="f-col">
                             <p className='text-light'>Community</p>
-                            <p><Link to='#'>News</Link></p>
-                            <p><Link to='#'>Blog</Link></p>
-                            <p><Link to='/contact'>Our Offices</Link></p>                        
+                            <p><Link to='#' onClick={scrollToTop}>News</Link></p>
+                            <p><Link to='#' onClick={scrollToTop}>Blog</Link></p>
+                            <p><Link to='/contact' onClick={scrollToTop}>Our Offices</Link></p>                        
                         </div>
                     </div>
 
@@ -53,13 +82,18 @@ export default function Footer() {
                             </div>
                         </div>
                     </div>
+                    {isVisible && (
+                        <div onClick={scrollToTop} className="scroll-to-top-btn">
+                            <i className="bi bi-arrow-up-circle-fill text-light"></i>
+                        </div>
+                    )}
                     <hr/>
                     <div className="col-md-12"><hr className='bg-secondary'/>
                         <div className='copy'>Copyright 2023 &copy; ALMS Global Immigration Consultant Inc. All rights reserved.</div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div>        
     </div>
   )
 }
